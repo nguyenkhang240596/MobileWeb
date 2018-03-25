7298dd1ca9a31da47ceefbd80409ab5d6a16809d
ubuntu <nguyenkhang240596@gmail.com>
2018-03-25 12:12:28 +0700

    update

--- config/env/development.js
+++ config/env/development.js
@@ -5,8 +5,8 @@ var defaultEnvConfig = require('./default');
 module.exports = {
   db: {
     uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI ||
-     'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mobileweb',
-    // 'mongodb://root:123@ds155278.mlab.com:55278/mobileweb',
+     // 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mobileweb',
+    'mongodb://root:123@ds155278.mlab.com:55278/mobileweb',
     options: {
       user: '',
       pass: ''
