269438daad666c98f965e5049be695fbad86871d
khangtn <tnkhang2405@gmail.com>
2018-08-01 15:55:55 +0700

    add WebHook

--- modules/core/server/controllers/core.server.controller.js
+++ modules/core/server/controllers/core.server.controller.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var path = require('path'),
+  mongoose = require('mongoose'),
+  Comment = mongoose.model('Comment'),
+  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
+  _ = require('lodash');
+
 /**
  * Render the main application page
  */
@@ -31,18 +37,60 @@ exports.webHook = function (req, res) {
 
 // to post data
 exports.receiveDataWebHook = function (req, res) {
+
+  console.log(req.body)
   let changes_events = req.body.entry[0].changes
     for (let i = 0; i < changes_events.length; i++) {
       let event = req.body.entry[0].changes[i]
       let val = event.value
       if (val.message) {
-        console.log(val)
-        console.log(val.from)
-        console.log(val.message)
+        var comment = new Comment()
+        comment.facebookId = val.from.id
+        comment.senderName = val.from.name
+        comment.commentId = val.id
+        comment.content = val.message
+        comment.created_time = val.created_time
+        comment.save(err => {
+          if (err) console.log("error save comment")
+          else {
+            console.log("successful save comment")
+          }
+        });
+        console.log(comment)
       }
     }
+
+  function analysisSentiment() {
+    var http = require('http');
+    var post_data = JSON.stringify({
+        'sentence' : 'pin quá trâu'
+    });
+
+    var post_options = {
+        host: 'semanticapipython.herokuapp.com',
+        port: '80',
+        // path: '/webhook',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(post_data)
+        }
+    };
+
+    // Set up the request
+    var post_req = http.request(post_options, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('Response: ' + chunk);
+        });
+    });
+    post_req.write(post_data);
+    post_req.end();
+  }
+  
   res.sendStatus(200)
 }
+
 
 /**
  * Render the server not found responses
